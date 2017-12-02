03f0f5f44911d04a5858183af201f6bcca4de776
Deven Walia <deven@docon.co.in>
2017-12-03 03:55:38 +0530

    trails, titling check added

--- microservices/ui/src/app/app.component.ts
+++ microservices/ui/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RoundProgressModule } from 'angular-svg-round-progressbar';
+import { Http} from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -25,15 +26,43 @@ export class AppComponent {
   			'action': 'bought',
   			'nameTo' : 'Mulayam singh yadav'
   		  }];
+  optionsMeta = {
+  				'id1' : {'name' : 'Ghatkopar, Deven Walia, plot786'},
+             	'id2' : {'name' : 'Shalimar Bagh, Anil Walia, plot3110'},
+             	'id3' : {'name' : 'Punjabi Bagh, Kamaljit Walia, plot0210'},
+             	'id4' : {'name' : 'Bohra ganesh chawk, Mohanlal Sukhodia, plot111'}
+             };
+  trailsMeta =  {
+  				'id1' : 
+  					[{'name' : 'Nikola Tesla'},
+	             	{'name' : 'Anil Walia'},
+	             	{'name' : 'Deven Walia'},
+	             	{'name' : 'John Lennon'},
+	             	{'name' : 'Marshall Mathers Eminem'}],
+	             'id2' : 
+  					[{'name' : 'Narendra Singh Modi'},
+	             	{'name' : 'Mayawati'},
+	             	{'name' : 'Anil Walia'}],
+	             'id3' : 
+  					[{'name' : 'Princess Diana'},
+	             	{'name' : 'Kamaljit Walia'}],
+	             'id4' : 
+  					[{'name' : 'Baba Ram Rahim Singh Insaan'},
+	             	{'name' : 'Mohanlal Sukhodia'}]
+             };
   txnsText = '';
-  current = 5;
+  current = 3;
   showRPB = false;
   RBPInterval = null;
   searchText = '';
-  options = [{'name' : 'Ghatkopar, Deven Walia, plot786'},
-             {'name' : 'Shalimar Bagh, Anil Walia, plot3110'},
-             {'name' : 'Punjabi Bagh, Kamaljit Walia, plot0210'},
-             {'name' : 'Bohra ganesh chawk, Mohanlal Sukhodia, plot111'}];
+  showTrail = false;
+  options = [];
+  trails = [];
+  isErr = false;
+  showErr = false;
+  errText = '';
+
+  constructor(public http: Http) {}
 
   ngOnInit() {
   	let x = [];
@@ -48,21 +77,45 @@ export class AppComponent {
   		gap += '&nbsp;'
   	}
   	this.txnsText = x.join(gap);
+  	this.options.length = 4;
   }
 
-  search(option=null) {
-  	this.current = 5;
+  search(i=null) {
+  	this.current = 3;
   	this.showRPB = true;
+  	this.showTrail = false;
+  	this.showErr = false;
 
   	this.RBPInterval = setInterval(()=>{
   		this.current--;
-  		if(!this.current) {
+  		if(this.current == -1) {
   			clearInterval(this.RBPInterval);
   			this.showRPB = false;
+  			this.showTrail = true;
+  			this.current = 3;
+  			this.checkBenami(i);
   		}
   	}, 1000);
-  	if(option){
-  		this.searchText = option['name'];
+  	this.searchText = this.optionsMeta['id'+i]['name'];
+	this.trails = this.trailsMeta['id'+i];
+  	
+  	// let data = '';
+  	// let err = '';
+  	// this.http.get('https://api.balmy35.hasura-app.io', this.searchText).subscribe(data => {
+  	// 	console.log(data);
+  	// });
+  }
+
+  checkBenami(i) {
+  	if(i){	
+  		this.showErr = true;
+  		if(this.trails[this.trails.length-1]['name'] != (this.searchText.split(',')[1].trim())) {
+  			this.isErr = true;
+  			this.errText = 'Property has titling issue';
+  		} else {
+  			this.isErr = false;
+  			this.errText = 'Property doesnt have titling issue';
+  		}
   	}
   }
 }
